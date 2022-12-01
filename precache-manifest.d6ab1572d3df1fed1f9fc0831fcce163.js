28bf6fa49d5b92059786a7db2a574a57ab1362ec
zhao <zhaohuili0210@gmail.com>
2022-11-30 19:09:38 -0500

    Initial Commit

---
+++ precache-manifest.d6ab1572d3df1fed1f9fc0831fcce163.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1866575b8a93156478efa9413453421c",
+    "url": "/Task-App-Final/index.html"
+  },
+  {
+    "revision": "1772dfa40b520bd76522",
+    "url": "/Task-App-Final/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "c1d7011b303c92dda243",
+    "url": "/Task-App-Final/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "1772dfa40b520bd76522",
+    "url": "/Task-App-Final/static/js/2.06944839.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Task-App-Final/static/js/2.06944839.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1d7011b303c92dda243",
+    "url": "/Task-App-Final/static/js/main.ca347540.chunk.js"
+  },
+  {
+    "revision": "4ce0097937ffffcbe9eb",
+    "url": "/Task-App-Final/static/js/runtime-main.c1d7a095.js"
+  }
+]);
